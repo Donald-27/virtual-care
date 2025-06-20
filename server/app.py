7728fffb8e41bf1d1c6d56a04c7d1926f24baf37
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 14:39:55 +0300

    added doctors api route to return all doctor records

--- server/app.py
+++ server/app.py
@@ -3,14 +3,27 @@
 # Remote library imports
 from flask import request
 from flask_restful import Resource
-from models import Doctor, Patient, Appointment, DoctorNote, EmergencyRequest, Symptom, DoctorAvailability
+
 # Local imports
 from config import app, db, api
+from models import Doctor, Patient, Appointment, DoctorNote, EmergencyRequest, Symptom, DoctorAvailability
 
+# Test route
 @app.route('/')
 def index():
     return '<h1>VirtualCare Server is Running</h1>'
 
-# Start the app
+# ----------- API Resources -----------
+
+class DoctorsResource(Resource):
+    def get(self):
+        doctors = Doctor.query.all()
+        return [doctor.to_dict() for doctor in doctors], 200
+
+# Register route
+api.add_resource(DoctorsResource, '/doctors')
+
+# ----------- Run App -----------
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
