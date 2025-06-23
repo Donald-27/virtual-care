668c607411fadb209702333b024583c853582bfb
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-23 07:23:36 +0300

    import appointment model in app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from models import Doctor, Patient, Appointment
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
