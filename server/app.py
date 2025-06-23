6e9ac681fa73c6b3da95710c8ba2d05ccda69069
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-23 07:30:43 +0300

    forgot to import resource in app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models import Doctor, Patient, Appointment
+from flask_restful import Resource
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
