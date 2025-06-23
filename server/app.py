36e46fda9c84a0b08baebba2399acee966c72bab
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-23 07:07:04 +0300

    added doctors and patient models with API models

--- server/app.py
+++ server/app.py
@@ -1,35 +1,65 @@
 #!/usr/bin/env python3
 
-from flask import request
-from flask_restful import Resource
+# Import the main libraries
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-from config import app, db, api
-from models import Doctor, Patient
+# Create the Flask app
+app = Flask(__name__)
 
+# Connect to the database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"  # You can change this if using another database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Create the database object
+db = SQLAlchemy(app)
+
+# ============ MODELS =============
+
+# Doctor model
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    department = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "department": self.department
+        }
+
+# Patient model
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
 @app.route('/')
-def index():
+def home():
     return '<h1>VirtualCare Server is Running</h1>'
 
-class DoctorsResource(Resource):
-    def get(self):
-        doctors = Doctor.query.all()
-        return [doctor.to_dict() for doctor in doctors], 200
+@app.route('/doctors', methods=['GET'])
+def get_doctors():
+    doctors = Doctor.query.all()
+    return jsonify([doctor.to_dict() for doctor in doctors])
 
-api.add_resource(DoctorsResource, '/doctors')
+@app.route('/patients', methods=['GET'])
+def get_patients():
+    patients = Patient.query.all()
+    return jsonify([patient.to_dict() for patient in patients])
 
-class PatientsResource(Resource):
-    def get(self):
-        patients = Patient.query.all()
-        return [patient.to_dict() for patient in patients], 200
-
-    def post(self):
-        data = request.get_json()
-        new_patient = Patient(name=data['name'])
-        db.session.add(new_patient)
-        db.session.commit()
-        return new_patient.to_dict(), 201
-
-api.add_resource(PatientsResource, '/patients')
+@app.route('/patients', methods=['POST'])
+def create_patient():
+    data = request.get_json()
+    new_patient = Patient(name=data['name'])
+    db.session.add(new_patient)
+    db.session.commit()
+    return jsonify(new_patient.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
