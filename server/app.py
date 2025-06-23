c2b6e660262bac516c7b6e3a206463ac01c07707
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-23 06:54:20 +0300

    added GET and POST endpoints for patients resource in app.py

--- server/app.py
+++ server/app.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
-# Remote library imports
 from flask import request
 from flask_restful import Resource
 
-# Local imports
 from config import app, db, api
-from models import Doctor, Patient, Appointment, DoctorNote, EmergencyRequest, Symptom, DoctorAvailability
+from models import Doctor, Patient
 
-# Test route
 @app.route('/')
 def index():
     return '<h1>VirtualCare Server is Running</h1>'
-
-# ----------- API Resources -----------
 
 class DoctorsResource(Resource):
     def get(self):
         doctors = Doctor.query.all()
         return [doctor.to_dict() for doctor in doctors], 200
 
-# Register route
 api.add_resource(DoctorsResource, '/doctors')
 
-# ----------- Run App -----------
+class PatientsResource(Resource):
+    def get(self):
+        patients = Patient.query.all()
+        return [patient.to_dict() for patient in patients], 200
+
+    def post(self):
+        data = request.get_json()
+        new_patient = Patient(name=data['name'])
+        db.session.add(new_patient)
+        db.session.commit()
+        return new_patient.to_dict(), 201
+
+api.add_resource(PatientsResource, '/patients')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
