ba416759db6f3a782b0a5b2f2a867220654d4755
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-30 12:05:29 +0300

    import jwt in app app.py

--- server/app.py
+++ server/app.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from flask_migrate import Migrate
 from config import db
 from models import Doctor, Patient, Appointment, EmergencyRequest, Symptom, DoctorNote
+from flask_jwt_extended import (
+    JWTManager, create_access_token, jwt_required, get_jwt_identity
+)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///virtualcare.db'
