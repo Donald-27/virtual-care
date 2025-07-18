bddfedc538cac82101e307a4a8ff27c856f5c415
Donald Kiprop <kipropdonald27@gmail.com>
2025-07-18 20:16:56 +0800

    Fix: run flask db upgrade at startup

--- server/app.py
+++ server/app.py
@@ -266,18 +266,9 @@ class PatientDoctorNotesResource(Resource):
 
 api.add_resource(PatientDoctorNotesResource, '/patients/<int:patient_id>/doctor-notes')
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-import os
-
-if __name__ == '__main__':
-    from flask_migrate import upgrade
-    with app.app_context():
-        upgrade()
-    app.run()
 from flask_migrate import upgrade
 
 if __name__ == "__main__":
     with app.app_context():
-        upgrade()
-    app.run()
+        upgrade()  
+    app.run(host="0.0.0.0", port=5555, debug=True)
